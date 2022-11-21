c3c5c03ba8a6d1eb88d53d6f661e12be6936ad26
mloubout <mathias.louboutin@gmail.com>
2022-11-21 06:37:15 -0500

    new packages

--- read_csv.py
+++ read_csv.py
@@ -1,9 +1,10 @@
 import csv
 
-with open('attendance.csv', 'r', newline='') as csvfile:
-    lines = csv.reader(csvfile, delimiter = ',', quotechar = '|')
-    for line in lines:
-        try:
-           print([c for c in line if '@' in c][0].strip())
-        except IndexError:
-            pass
+for registered in ['attendance.csv', 'remotes-users.csv']:
+    with open(registered, 'r', newline='') as csvfile:
+        lines = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+        for line in lines:
+            try:
+                print([c for c in line if '@' in c][0].strip())
+            except IndexError:
+                pass
